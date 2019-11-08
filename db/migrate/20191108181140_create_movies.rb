8f9e23abbf09cfe6482286eb33b2525391f80599
James Mobus <mobusj@live.com>
2019-11-08 18:12:55 +0000

    Automatically backed up by Learn

--- db/migrate/20191108181140_create_movies.rb
+++ db/migrate/20191108181140_create_movies.rb
@@ -1,10 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+      t.string :title 
+      t.integer :release_date
+      t.string :director 
+      t.string :lead
+      t.boolean :in_theaters
     end
   end
 end
